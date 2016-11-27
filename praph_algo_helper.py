d974922704c3f9a25f45cb489090c5354288b526
adi319 <adi319@users.noreply.github.com>
2016-11-27 16:10:44 +0200

    Update praph_algo_helper.py

--- praph_algo_helper.py
+++ praph_algo_helper.py
@@ -1,47 +1,110 @@
-from graph_algo import grpahAlgo
-import getopt
-import sys
-import os
+# Graph Algorithms using Python Networkx
+# Application Structures, Assignment 1, Semester A
+# Authors: Dudu Bistrov, Adiel Amshalom, Moshe Margaliot
+# 21, November 2016
 
-def usage():
-    print """Command line usage:
+import networkx as nx
+import time
 
-    -h, --help          This help screen
-    -i, --init-file     Use CM (config management) default values
-    -t, --test-file       Don't test after installation
 
-    """
+class grpahAlgo:
 
-try:
-    opts, args = getopt.getopt(sys.argv[1:], "hi:t:", ["help", "init-file=", "test-file="])
-except getopt.GetoptError as err:
-    # print help information and exit:
-    print str(err) # will print something like "option -a not recognized"
-    usage()
-    sys.exit(2)
+    G = ''
 
-init_file_path = ''
-test_file_path = ''
+    def __init__(self):
+        self.G = nx.Graph()
 
-for o, a in opts:
-    if o in ("-h", "--help"):
-        usage()
-        sys.exit()
-    elif o in ("-i", "--init-file"):
-        if (os.path.isfile(a)):
-            init_file_path = a
-        else:
-            print a + "input file not found"
-            exit(1)
-    elif o in ("-t", "--test-file"):
-        if (os.path.isfile(a)):
-            test_file_path = a
-        else:
-            print a + "test file not found"
-            exit(1)
-    else:
-        assert False, "unhandled option"
+    def init_graph_from_file(self,file):
 
-GG = grpahAlgo()
-GG.init_graph_from_file(init_file_path)
-GG.test_graph(test_file_path)
+        init_file_object = open(file, 'r')
+        # TO DO: write validation test
+        lines = init_file_object.readlines()
+
+        number_of_nodes = lines[0]
+        number_of_edges = lines[1]
+
+        del lines[0:2]
+
+        for line in lines:
+            _node_i = int(line.split()[0])
+            _node_j = int(line.split()[1])
+            _weight = float(line.split()[2])
+            self.G.add_weighted_edges_from([(_node_i, _node_j, _weight), ])
+
+        init_file_object.close()
+        return self.G
+
+    def test_graph(self,file):
+
+        start = time.clock()
+        test_file_object = open(file, 'r')
+        lines = test_file_object.readlines()
+
+        del lines[0:1]
+
+        for line in lines:
+            line_args = line.split(' ')
+            if (str(line_args[0]) == "info"):
+                print("Graph: |V|=" + str(self.G.number_of_nodes()) +
+                       ", |E|=" + str(self.G.number_of_edges())     +
+                       ", TIE=" + str(self.triangle_check())        +
+                       ", Radius:" + str(self.get_rad())            +
+                       ", Diameter:" + str(self.get_diamiter())     +
+                       ", Runtime:" + str(time.clock()-start))
+
+            elif (int(line_args[2]) == 0):
+                print ("%s %.2f" % (line.strip('\n'), self.get_path_length(line_args[0], line_args[1])))
+
+            elif (int(line_args[2]) > 0):
+                bl_list = line_args[3:]
+                orig_list = []
+                for item in bl_list:
+                    n_list = nx.neighbors(self.G,int(item))
+                    for n in n_list:
+                        ed = str(self.G.get_edge_data(int(item), n))[11:-1]
+                        t_list = [int(item), int(n), float(ed)]
+                        orig_list.append(t_list)
+                        self.G.add_weighted_edges_from([(int(item), int(n), float("inf")), ])
+
+                print ("%s %.2f" % (line.strip('\n'), self.get_path_length(line_args[0], line_args[1])))
+
+                for list in orig_list:
+                    self.G.add_weighted_edges_from([(int(list[0]), int(list[1]), float(list[2])), ])
+
+
+    def get_blacklist_graph(self,bl_list):
+        bl_G=nx.Graph(self.G);
+
+        for item in bl_list:
+            bl_G.remove_node(int(item))
+
+        return bl_G
+
+    def get_path_length(self,_source,_dest):
+        return nx.dijkstra_path_length(self.G, source=int(_source), target=int(_dest), weight="weight")
+
+    def get_path(self,_source,_dest):
+        return nx.dijkstra_path(self.G, source=int(_source), target=int(_dest), weight="weight")
+
+    def get_diamiter(self):
+        return nx.diameter(self.G)
+
+    def get_rad(self):
+        return nx.radius(self.G)
+
+    def triangle_check(self):
+        triangle = [circle for circle in nx.cycle_basis(self.G)]
+        for item in triangle:
+            a = float(str(self.G.get_edge_data(item[0],item[1]))[11:-1])
+            b = float(str(self.G.get_edge_data(item[1],item[2]))[11:-1])
+            c = float(str(self.G.get_edge_data(item[2],item[0]))[11:-1])
+
+            if ((a>b+c) or (b>a+c) or (c>a+b)):
+                return False
+        return True
+
+    def print_graph(self):
+        for n, nbrs in self.G.adjacency_iter():
+            for nbr, eattr in nbrs.items():
+                data = eattr['weight']
+                print('(%d, %d, %.2f)' % (n, nbr, data))
